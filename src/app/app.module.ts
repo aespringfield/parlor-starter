9f1765f7260da7c2f60b32408ea25c07eae9a1fb
Anna Springfield <anna.e.springfield@gmail.com>
2017-09-13 22:58:57 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http'
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -6,18 +7,30 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Config } from './config'
+
+import { AsyncPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(Config.FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  items: FirebaseListObservable<any[]>;
+  timbo: FirebaseObjectObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+    this.items = afDB.list('/Animals');
+    this.timbo = afDB.object('/Timbo');
   }
 
+  ngOnInit(): void {
+    console.log(this.items);
+    console.log(this.timbo);
+  }
 }
